ed1ffb51e8b3040c0ceb8c6ce9875ae77f617c23
Wessel van Hemert <wessel.vanhemert@soprasteria.com>
2025-06-16 20:59:53 +0200

    file path change

--- DI-Validator-Tester/SolutionAnalyzerTests.cs
+++ DI-Validator-Tester/SolutionAnalyzerTests.cs
@@ -15,7 +15,7 @@ namespace DI_Validator_Tester
         public async Task AnalyzeSolution()
         {
             // Arrange
-            var solutionPath = @"G:\DEV\repos\sopra\DIAssertionsCasus-master\DIAssertionsCasus.sln";
+            var solutionPath = @"C:\Users\wvanhemert\source\repos\DIAssertionsCasus\DIAssertionsCasus.sln";
             var config = new DI_Validator_Analyzers.Models.SolutionAnalysisConfig
             {
                 SolutionPath = solutionPath,
