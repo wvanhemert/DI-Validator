ff8bef6a3ba9b6e1f751f3ff5e51b386765d7e86
Wessel van Hemert <wessel.vanhemert@soprasteria.com>
2025-06-25 13:51:00 +0200

    user test

---
+++ DI-Validator-Tester/UserTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DI_Analyzer_TestHelpers;
+using DI_Validator_Analyzers.Models;
+using Microsoft.CodeAnalysis;
+
+namespace DI_Validator_Tester
+{
+    [TestClass]
+    public class UserTest
+    {
+        [TestMethod]
+        public async Task AnalyzeSolution()
+        {
+            var config = new SolutionAnalysisConfig
+            {
+                SolutionPath = @"",
+                SeverityFilter = new[] { DiagnosticSeverity.Warning, DiagnosticSeverity.Error, DiagnosticSeverity.Info },
+                EnableLogging = false,
+                FailOnInfo = false,
+            };
+            await AnalyzerTestRunner.AssertSolution(config);
+
+        }
+    }
+}
